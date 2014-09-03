8a12147f83d4502dd2bebc08f836398e04848005
Andrius Bentkus <andrius.bentkus@gmail.com>
2014-09-03 12:04:02 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# galaxy-mysql
+
+This is a [galaxy](https://github.com/bjouhier/galaxy) wrapper for [mysql](https://github.com/felixge/node-mysql).
+
+License
+=======
+
+This library is available under the [MIT license](http://en.wikipedia.org/wiki/MIT_License).
---
+++ main.js
@@ -0,0 +1,48 @@
+(function () {
+	var galaxy = require('galaxy');
+	var mysql  = require('mysql');
+
+	require('galaxy-nova');
+
+	function augmentConnection(connection) {
+		connection.queryAsync   = galaxy.star(galaxy.nova(connection, connection.query, ["rows", "fields"]));
+
+		['connect', 'ping', 'statistics', 'end'].forEach(function (obj) {
+			connection[obj + 'Async'] = galaxy.star(connection[obj]);
+		});
+
+		return connection;
+	}
+	
+
+	galaxy.augment(mysql, "createConnection", augmentConnection);
+
+	galaxy.augment(mysql, "createPool", function (pool) {
+
+		galaxy.augment(pool, "getConnection", null, augmentConnection);
+
+		pool.getConnectionAsync = galaxy.star(pool.getConnection);
+	});
+
+
+	if (module.parent) {
+		module.exports = mysql;
+		return;
+	}
+	
+	var connection = mysql.createConnection(
+		JSON.parse(require('fs').readFileSync('mysql.json', 'utf8'))
+	);
+
+	galaxy.main(function *() {
+		try {
+			connection.connect();
+			var res = yield connection.queryAsync('SELECT 1 + 1 AS solution');
+			console.log(res.rows[0].solution);
+			console.log(yield connection.statisticsAsync());
+			connection.end();
+		} catch (ex) {
+			console.log(ex);
+		}
+	});
+})();
